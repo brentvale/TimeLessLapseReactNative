99144dc0810f40433a9a69d86576cd2d3aee6855
Brent Vale <brentvale@gmail.com>
2019-01-13 07:27:27 -0800

    plays through sequence of images after upload

--- js/components/screens/photo/upload.js
+++ js/components/screens/photo/upload.js
@@ -15,12 +15,34 @@ export default class Upload extends Component {
     super();
     this.state = {
       images: [],
+      currentIndex: 0,
     };
   }
 
   options = {
     mediaType: 'photo',
     multiple: true,
+    includeExif: true,
+    maxFiles: 20, // iOS only
+    forceJpg: true, // Live Photo will be converted to its JPG representation
+  };
+
+  componentWillUnmount(){
+    if(this.playInterval){
+      clearInterval(this.playInterval);
+    }
+  }
+
+  play = () => {
+    this.playInterval = setInterval(() => {
+      this.setState({
+        currentIndex: this.state.images[this.state.currentIndex + 1] ? this.state.currentIndex + 1 : 0,
+      });
+    }, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.playInterval);
   };
 
   onUploadPress = () => {
@@ -37,35 +59,99 @@ export default class Upload extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.uploadButton}
-            onPress={() => this.onUploadPress()}>
-            <Text>Upload</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 4 }}>
+        {!(this.state.images.length)
+        && <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.uploadButton}
+              onPress={() => this.onUploadPress()}>
+              <Text>Upload</Text>
+            </TouchableOpacity>
+          </View>}
+        <View style={{ flex: 5 }}>
           {Boolean(this.state.images.length)
-            && <ScrollView
-                  style={styles.scroll}
-                >
-                  <View style={styles.scrollInner}>
-                    {this.state.images.map(imgObj => <ImageDisplay imgObj={imgObj} key={imgObj.filename}/>)}
-                  </View>
-               </ScrollView>}
+          && <View style={{ flex: 1 }}>
+              <View style={{ flex: 3 }}>
+                <ImagePlayThrough images={this.state.images}
+                                  currentIndex={this.state.currentIndex}
+                />
+              </View>
+              <View style={{ flex: 1 }}>
+                <PlayAndPauseRow play={this.play}
+                                 pause={this.pause}
+                />
+              </View>
+              <View style={{ flex: 6 }}>
+                <ImageListScrollView images={this.state.images}
+                                     currentIndex={this.state.currentIndex}
+                />
+              </View>
+            </View>}
         </View>
       </View>
     );
   }
 }
-
-const ImageDisplay = ({ imgObj }) => (
-  <View style={{ height: 100, width: 100, margin: 2 }}>
-    <Image source={{ uri: imgObj.sourceURL }}
-           style={styles.imageContainer}
-           resizeMode={'contain'}
-    />
+const ImagePlayThrough = ({ images, currentIndex }) => (
+  <View style={{ flex: 1, backgroundColor: 'orange' }}>
+    <ImagePlay imgObj={images[currentIndex]} />
   </View>
+);
+
+const PlayAndPauseRow = ({ play, pause }) => (
+  <View style={{ flex: 1, flexDirection: 'row' }}>
+    <View style={styles.left}>
+      <TouchableOpacity
+        style={styles.controlButton}
+        onPress={() => play()}
+      >
+        <Text>Play</Text>
+      </TouchableOpacity>
+    </View>
+    <View style={styles.right}>
+      <TouchableOpacity
+        style={styles.controlButton}
+        onPress={() => pause()}
+      >
+        <Text>Pause</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const ImageListScrollView = ({ images, currentIndex }) => (
+  <ScrollView
+    style={styles.scroll}
+  >
+    <View style={styles.scrollInner}>
+      {images.map((imgObj, idx) => <ImageDisplay imgObj={imgObj}
+                                                 key={imgObj.filename}
+                                                 currentIndex={currentIndex}
+                                                 index={idx}
+      />)}
+    </View>
+  </ScrollView>
+);
+
+const ImageDisplay = ({ imgObj, currentIndex, index }) => {
+  const borderColor = currentIndex === index ? 'blue' : 'white';
+  return (
+    <View style={{ height: 100, width: 100, margin: 2 }}>
+      <Image source={{ uri: imgObj.sourceURL }}
+             style={[
+               styles.imageContainer,
+               { borderColor },
+             ]}
+             resizeMode={'contain'}
+      />
+    </View>
+  );
+};
+
+const ImagePlay = ({ imgObj }) => (
+  <Image source={{ uri: imgObj.sourceURL }}
+         style={{ flex: 1 }}
+         resizeMode={'contain'}
+  />
 );
 
 const styles = StyleSheet.create({
@@ -76,6 +162,26 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     height: '100%',
+    borderWidth: 1,
+  },
+  left: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  controlButton: {
+    height: '100%',
+    width: 80,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
   scroll: {
     flex: 1,
